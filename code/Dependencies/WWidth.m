9d5e48d095e6a030d9df924778a7e271791934f7
Connor <connor.healy@utah.edu>
2020-08-14 11:15:19 -0600

    Added description

--- code/Dependencies/WWidth.m
+++ code/Dependencies/WWidth.m
@@ -1,5 +1,5 @@
 function [WindowWidth,WindowHeight] = WWidth(mask,nFrames,fOverlap)
-%UNTITLED3 Summary of this function goes here
+%WWIDTH Compute window width. 
 %   Detailed explanation goes here
 [WindowHeight,MaskWidth] = size(mask);
 WindowWidth = MaskWidth/(nFrames-nFrames*fOverlap+fOverlap);
