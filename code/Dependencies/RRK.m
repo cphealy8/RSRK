b367e563d2c287d39356c18b7ca03cb486620022
Connor <connor.healy@utah.edu>
2020-08-19 15:56:37 -0600

    Tweaked storage format. RRK results are now stored in matrices. Rows correspond to scales, columns correspond to window position, and the depth corresponds to the simulation. Results for separate window schemes are stored in a cell array.

--- code/Dependencies/RRK.m
+++ code/Dependencies/RRK.m
@@ -1,10 +1,11 @@
-function [K,LminR] = RRK(pts,win,r,nFrames,fOverlap,EdgeCorrection)
+function [K,LminR,FPosition] = RRK(pts,win,r,nFrames,fOverlap,EdgeCorrection)
 %RRK Rolling Ripley's K
 %   Detailed explanation goes here
 
 % Compute Window Width
 WinHeight = win(4)-win(3);
 WinWidth = win(2)-win(1);
+rLen = length(r);
 
 % Compute Frame Dimensions
 FWidth = WinWidth/(nFrames-nFrames*fOverlap+fOverlap);
@@ -12,18 +13,21 @@ FWidth = floor(FWidth);
 
 FStarts = linspace(win(1),WinWidth-FWidth,nFrames);
 FEnds = linspace(FWidth,win(2),nFrames);
+FPosition = FEnds./WinWidth; % Frame position as fraction of window width.
 
 % Preallocate
-K = cell(1,nFrames);
-LminR = cell(1,nFrames);
+K = zeros(nFrames,rLen);
+LminR = zeros(nFrames,rLen);
 
 for n=1:nFrames
     CurFrame = [FStarts(n) FEnds(n) win(3) win(4)];
     CurPts = CropPts2Win(pts,CurFrame);
     
-    [K{n},~,L] = Kest(CurPts,CurFrame,'t',r,'EdgeCorrection',EdgeCorrection);
-    LminR{n} = L-r;
+    [K(n,:),~,L] = Kest(CurPts,CurFrame,'t',r,'EdgeCorrection',EdgeCorrection);
+    LminR(n,:) = L-r;
+    
 end
-
+K = K';
+LminR = LminR';
 end
 
