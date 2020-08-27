c9140e479634b38459998ea0bc111c750f4f5b77
Connor <connor.healy@utah.edu>
2020-08-27 08:58:15 -0600

    Generates plots of each verification pattern for each window state. Plots show the average of L(r) -r for a given scale (r) and window across the simulations.
    Need  a way to illustrate variability in these...hmm

--- code/RRK_Verification_AnalysisAndPlots.m
+++ code/RRK_Verification_AnalysisAndPlots.m
@@ -9,25 +9,42 @@
 
 %% Normalizing
 clc; clear; close all;
-% load('..\data\Verification Tests\PP06 - Mixed - Homogenous.mat')
-load('..\data\Verification Tests\PP03 - Aggregated - Large Clusters - Homogenous.mat')
-KTarget = K;
-clear K L
+dirname = '..\data\Verification Tests\';
+fnames = dir(dirname);
+SaveDir = '..\results\Verification Tests';
+minL = [];
+maxL = [];
+for n = 3:length(fnames)
+clearvars -except dirname fnames n SaveDir minL maxL
+
+filename = fnames(n).name;
+fulldir = fullfile(dirname,fnames(n).name);
+load(fulldir)
+% KTarget = K;
+
 
 % Test Stat
-load('..\data\Verification Tests\PP01 - Random Homogenous.mat')
-KTest = K;
-clear K L
+% load('..\data\Verification Tests\PP01 - Random Homogenous.mat')
+% KTest = K;
+% clear K L
 
 %% Comparison 
-G = RRK_Log2Norm(KTarget,KTest);
-% % means = cell2mat(RRK_Mean(G));
-% % minG = round(min(means(:)),2)
-% % maxG = round(max(means(:)),2)
+% G = RRK_Log2Norm(KTarget,KTest);
+means = cell2mat(RRK_Mean(L));
+minL(n-2) = min(means(:));
+maxL(n-2) = max(means(:));
 
 % P = RRK_TTest2(KTarget,KTest);
 % stat = RRK_ComparisonStat(KTarget,KTest);
 
-[fH,pH] = RRK_Verification_Plot(r,FPosition,KTarget);
+[fH,pH] = RRK_Verification_Plot(r,FPosition,L,pts,PPName);
 xlabel('Window Position');
 ylabel('Scale (r)');
+
+SaveName = fullfile(SaveDir, filename(1:end-4));
+
+saveas(fH,SaveName,'pdf')
+print(fH,SaveName,'-dpng')
+
+close all;
+end
