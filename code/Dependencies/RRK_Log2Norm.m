c98a9ed7a7f0f3a7616899587345701353748e50
Connor <connor.healy@utah.edu>
2020-08-27 08:59:57 -0600

    Compute the Log2 of RRK results normalized to another set of RRK results (usually CSR).

---
+++ code/Dependencies/RRK_Log2Norm.m
@@ -0,0 +1,19 @@
+function [G] = RRK_Log2Norm(KTarget,KTest)
+%RRK_LOG2NORM Compute log2 of KTarget normalized to KTest.
+%   Detailed explanation goes here
+
+if iscell(KTarget)
+    for n=1:length(KTarget)
+        G{n} = Log2Norm(KTarget{n},KTest{n});
+    end
+else
+    G = Log2Norm(KTarget,KTest);
+end
+
+end
+
+function [G] = Log2Norm(KNum,KDenom)
+    G = log2(KNum./KDenom);
+end
+
+
