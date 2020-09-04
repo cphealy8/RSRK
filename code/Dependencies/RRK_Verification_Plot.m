c498cce157ec96610e072221288966e9965474c5
Connor <connor.healy@utah.edu>
2020-09-04 10:02:44 -0600

    Plots TTest results from RRK_TTest2

--- code/Dependencies/RRK_Verification_Plot.m
+++ code/Dependencies/RRK_Verification_Plot.m
@@ -1,4 +1,4 @@
-function [fH,pH] = RRK_Verification_Plot(r,FPosition,L,pts,PPName,cLims)
+function [fH,axL,axM] = RRK_Verification_Plot(r,FPosition,L,pts,PPName,cLims,T)
 %RRK_VERIFICATION_PLOT Plot RRK Verification Results
 %   Detailed explanation goes here
 %%
@@ -13,84 +13,80 @@ MeanMat = cell2mat(LMean);
 MaxMean = max(MeanMat(:));
 MinMean = min(MeanMat(:));
 
-fH = figure('Position',[50 50 350 50*length(L)]);
+fH = figure('Position',[50 50 500 100*length(L)]);
 [ha,pos] = tight_subplot(length(L)+1, 1, 0, 0.1, 0.15);
 
 axes(ha(1))
 plot(pts(:,1),pts(:,2),'.r','MarkerSize',5);
+axp = gca;
+axp.XTick = [];
+axp.YTick = [];
+
 title(PPName)
 for k=1:length(L)
 Lmean = mean(L{k},3,'omitnan');
 x = FPosition{k};
-if length(x)>1
-    diffx = x(2)-x(1);
-    xplot = [x(1)-diffx x];
-else
-    xplot = [0 x];
-end
+curT = T{k};
 
-rplot = [0 r];
-% xplot = [x];
-% rplot = [r];
-
-% f2 = figure('Units','points','Position',[300 300 400 150]);
-
-dr = log10(r(2))-log10(r(1));
-
-Lplot = [Lmean,zeros(size(Lmean,1),1); zeros(1,size(Lmean,2)+1)];
-
-% if strcat(cLims,'pval')
-%     PVAL = [L,zeros(size(L,1),1); zeros(1,size(L,2)+1)];
-% end
-
-
-% Lplot = Lmean;
 axes(ha(k+1));
 
+r=round(r,2);
+x=round(x,2);
+[axL(k),axM(k)] = imagescGrid(x,r,Lmean);
 
-% if strcmp(cLims,'pval')
-%     PVals = 1-Lplot;
-%     Lplot(PVals>=0.999) = 4;
-%     Lplot(PVals<0.999) = 3;
-%     Lplot(PVals<0.99) = 2;
-%     Lplot(PVals<0.95) = 1;
+axes(axM(k))
+
+xL = axM(k).XLim;
+yL = axM(k).YLim;
+
+xctrs = linspace(xL(1),xL(2),2*length(x)+1);
+xctrs = xctrs(2:2:end);
+
+yctrs = linspace(yL(1),yL(2),2*length(r)+1);
+yctrs = yctrs(2:2:end);
+
+[xx,yy] = meshgrid(xctrs,yctrs);
+sx = xx(:);
+sy = yy(:);
+
+% apply significance test results
+Sig = curT<0.01;
+sx(Sig) = [];
+sy(Sig) = [];
+hold on
+plot(sx,sy,'xk');
+
+% pH=heatmap(x,r,Lmean);
+
+% Ax = gca;
+% Ax.CellLabelColor = 'none';
+% Ax.XDisplayLabels = nan(size(Ax.XDisplayData));
+% Ax.YDisplayLabels = nan(size(Ax.YDisplayData));
+%%% pcolor stuff %%%
+
+% if length(x)>1
+%     diffx = x(2)-x(1);
+%     xplot = [x(1)-diffx x];
+% else
+%     xplot = [0 x];
 % end
+% 
+% rplot = [0 r];
+% dr = log10(r(2))-log10(r(1));
+% Lplot = [Lmean,zeros(size(Lmean,1),1); zeros(1,size(Lmean,2)+1)];
 
-% pH = imagesc(x,r,Lmean);
-pH = pcolor(xplot,rplot,Lplot);
-
-% shading faceted
-set(gca,'YScale','log')
-YTicks = round(rplot(1:2:end),2);
-% ylim([10^(log10(r(1))-dr) r(end)])
-ylim([r(1) 10^(log10(r(end))+dr)])
-set(gca,'YTick',YTicks)
-YTickLabels = cellstr(num2str(round(log10(YTicks(:)),1), '10^%0.1f'));
-set(gca,'YTickLabel',YTickLabels);
-% xlabel('Window Position (% of Total Length)','FontSize',fntsz)
-% ylabel('Scale r (a.u.)','FontSize',fntsz)
-% zlabel('K(r)');
+% pH = pcolor(xplot,rplot,Lplot);
+% set(gca,'YScale','log')
+% YTicks = round(rplot(1:2:end),2);
+% 
+% ylim([r(1) 10^(log10(r(end))+dr)])
+% set(gca,'YTick',YTicks)
+% YTickLabels = cellstr(num2str(round(log10(YTicks(:)),1), '10^%0.1f'));
+% set(gca,'YTickLabel',YTickLabels);
 
 
-% ax = gca;
-% ax = TightAxes(ax);
-% set(ax,'YScale','log')
-% set(ax,'FontSize',fntsz)
-% c1 = colorbar;
-% Set axis limits to 3 times the maximum standard deviation of the
-% observations.
 
-% caxis(3*[-MaxSD MaxSD]+[MinMean MaxMean]);
-
-% caxis([-0.12327 0.12327]);
-
-% colInt = [5 33 67 146 209 247 253 244 214 178 103;...
-%           48 102 147 197 229 247 219 165 96 24 0;...
-%           97 172 195 222 240 247 199 130 77 43 31]';
-% map = myDivergingMap(colInt,10);
-
-
-%%%
+%%
 if strcmp(cLims,'pval')
     caxis([0 1])
     colInts = [0 0.05 0.0500001 1];
@@ -102,13 +98,10 @@ else
     colNames = {'#ca0020','#f7f7f7','#0571b0'};
 end
 
-%%% Use these when using the RRK_ComparisonStat
-% caxis([-1 1])
-% colInts = [0 0.0250 0.5 0.975 1];
-% colNames =  {'#2b83ba','#abdda4','#ffffbf','#fdae61','#d7191c'};
+%% Use these when using the RRK_ComparisonStat
 map = customcolormap(colInts,colNames);
 colormap(map)
+colorbar('off')
 end
-
 end
 
