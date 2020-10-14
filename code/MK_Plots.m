16e9301e0d5d0c995f9d69ad7e64d4e44e036930
Connor <connor.healy@utah.edu>
2020-10-14 09:28:40 -0600

    Code for plotting results from Megakaryocyte analysis.

---
+++ code/MK_Plots.m
@@ -0,0 +1,23 @@
+clc; clear; close all;
+[datFile,datPath] = uigetfile('../data/*mat','Load Data File');
+load(fullfile(datPath,datFile));
+
+%%
+sigLvl = 0.05;
+nr = length(r);
+nframes = length(FPosition);
+G = zeros(nr,nframes);
+IsSig = zeros(nr,nframes);
+
+FPosition= FPosition./(max(FPosition));
+
+for k=1:length(RK)
+    cRK = RK{k};
+    curK = cRK.Obs';
+    minEnv = quantile(cRK.CSRSims,sigLvl/2)';
+    maxEnv = quantile(cRK.CSRSims,1-sigLvl/2)';
+    G(:,k) = log2(cRK.Obs./cRK.CSR)';
+    IsSig(:,k) = (curK<=minEnv | curK>=maxEnv);
+end
+
+RSRK_Plot(r,FPosition,K,'SigMap',IsSig);
