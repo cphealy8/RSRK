f3761c331488bd4395a4bb0c690243546a878a11
Connor <connor.healy@utah.edu>
2020-10-14 14:35:04 -0600

    Now prepares and saves multiple plots per run.

--- code/MK_Plots.m
+++ code/MK_Plots.m
@@ -1,24 +1,64 @@
 clc; clear; close all;
-selpath = uigetdir('../data');
+addpath('Dependencies');
+dirname = '..\data\Kokliaris Dataset\';
+DirDat = dir(dirname);
+foldnames = {DirDat(3:end).name}';
+SaveDir = '..\results\Kokliaris Dataset\';
+
+% selpath = uigetdir('../data');
 % [datFile,datPath] = uigetfile('../data/*mat','Load Data File');
 % load(fullfile(datPath,datFile));
 
 %%
 sigLvl = 0.05;
-nr = length(r);
-nframes = length(FPosition);
-G = zeros(nr,nframes);
-IsSig = zeros(nr,nframes);
+cLims = [-0.3 0.3];
+cntr =0;
+%%
+for fID=1:length(foldnames)
+    curFold = fullfile(dirname,foldnames{fID});
+    curDirDat = dir(curFold);
+    curFiles = {curDirDat(3:end).name}';
+    isRKdata = contains(curFiles,'MKRK');
+    
+    RKdatafiles = curFiles(isRKdata);
+    
+    for datID=1:length(RKdatafiles)
+        curFile = RKdatafiles{datID};
+        load(fullfile(curFold,curFile));
+        nr = length(r);
+        nframes = length(FPosition);
+        G = zeros(nr,nframes);
+        IsSig = zeros(nr,nframes);
 
-FPosition= FPosition./(max(FPosition));
+        FPosition= FPosition./(max(FPosition));
 
-for k=1:length(RK)
-    cRK = RK{k};
-    curK = cRK.Obs';
-    minEnv = quantile(cRK.CSRSims,sigLvl/2)';
-    maxEnv = quantile(cRK.CSRSims,1-sigLvl/2)';
-    G(:,k) = log2(cRK.Obs./cRK.CSR)';
-    IsSig(:,k) = (curK<=minEnv | curK>=maxEnv);
+        for k=1:length(RK)
+            cRK = RK{k};
+            curK = cRK.Obs';
+            minEnv = quantile(cRK.CSRSims,sigLvl/2)';
+            maxEnv = quantile(cRK.CSRSims,1-sigLvl/2)';
+            G(:,k) = log2(cRK.Obs./cRK.CSR)';
+            IsSig(:,k) = (curK<=minEnv | curK>=maxEnv);
+        end
+        
+        cntr =cntr+1;
+        maxG(cntr) = max(G(:));
+        minG(cntr) = min(G(:));
+
+        [fH,axM,axH] = RSRK_Plot(r,FPosition,K,'SigMap',IsSig,'cLims',cLims);
+        SaveFile = curFile(1:end-4);
+        
+        ylabel(axH,'Scale r (µm)')
+        yticklabels(axH,r)
+        yticklabels(axM,[]);
+        xlabel(axM,'Window Position')
+        xticklabels(axM,[]);
+        
+        % Save results
+        SaveName = fullfile(SaveDir, SaveFile);
+        saveas(fH,SaveName,'pdf')
+        print(fH,SaveName,'-dpng');
+        
+        close all
+    end
 end
-
-RSRK_Plot(r,FPosition,K,'SigMap',IsSig);
