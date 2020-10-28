80f63b4f2a0f87cc3158cbdaf0144de3a1e0a524
Connor <connor.healy@utah.edu>
2020-10-28 11:39:19 -0600

    Analyze the spatial distribution of MKs (or other cells) with respect to each other using a rolling Ripley's K algorithm. Measures 2nd order spatial properties of cells

---
+++ code/RRK_MK_Analysis.m
@@ -0,0 +1,48 @@
+clc; clear; close all;
+addpath('Dependencies')
+%% Analysis Params
+nFrames = 15;
+fOverlap = 0.5;
+imscale = 1.50; % pix/µm
+SigLvl = 0.05;
+
+% Analysis scales (r);
+r = [5 10 15 20 30 50 100 150 200 300]; %[=] µm
+rscaled = r*imscale; %[=] pixels   
+
+Units = 'um';
+ScaleUnits = 'um/pixel';
+
+SaveDir = '..\results\Kokliaris Dataset\';
+
+%% Loading Mask and Point Data
+[MaskFile,MaskPath]  = uigetfile('../data/*bmp','Select Mask File');
+[PtFile,PtPath] = uigetfile(fullfile(MaskPath,'*mat'),'Select Points File');
+Mask = ~imread(fullfile(MaskPath,MaskFile));
+load(fullfile(PtPath,PtFile));
+
+%% Run Simulations
+[MaskHeight,MaskWidth] = size(Mask);
+win = [0 MaskWidth 0 MaskHeight];
+
+[G,Sig,~,FPosition,cLims,KScaled] = RRK_Norm(pts,rscaled,nFrames,fOverlap,SigLvl,'Mask',Mask);
+
+%% Plot
+[fH1,axM,axH] = RSRK_Plot(r,FPosition,G,'SigMap',Sig,'cLims',cLims);
+[fH2,axM,axH] = RSRK_Plot(r,FPosition,KScaled,'SigMap',Sig);
+%% Save the data
+
+FileName = strcat(PtFile(1:end-4),'_RRK','.mat');
+savedir = fullfile(MaskPath,FileName);
+save(savedir,'r','pts','Mask','imscale','nFrames','fOverlap','SigLvl',...
+    'G','Sig','FPosition','cLims','KScaled');
+% 
+
+% Save Figures
+SaveFileG = strcat(PtFile(1:end-4),'_RRK_G');
+SaveName = fullfile(SaveDir, SaveFileG);
+saveas(fH1,SaveName,'pdf')
+print(fH1,SaveName,'-dpng');
+
+S(1) = load('gong');
+sound(S(1).y,S(1).Fs)
