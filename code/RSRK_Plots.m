d63d9fe64998759073fd65fe306d1d5e1ea073fb
Connor <connor.healy@utah.edu>
2021-01-08 12:27:46 -0700

    Generic version of MK Plots with name changes to improve clarity and functionality.

---
+++ code/RSRK_Plots.m
@@ -0,0 +1,73 @@
+clc; clear; close all;
+addpath('Dependencies');
+dirname = '..\data\Kokliaris Dataset\';
+DirDat = dir(dirname);
+foldnames = {DirDat(3:end).name}';
+SaveDir = '..\results\Kokliaris Dataset\';
+
+%% Specs
+sigLvl = 0.05;
+cLims = [-0.3 0.3];
+cntr =0;
+%%
+for fID=1:length(foldnames)
+    curFold = fullfile(dirname,foldnames{fID});
+    curDirDat = dir(curFold);
+    curFiles = {curDirDat(3:end).name}';
+    isRKdata = contains(curFiles,'MKRK');
+    
+    RKdatafiles = curFiles(isRKdata);
+    
+    for datID=1:length(RKdatafiles)
+        curFile = RKdatafiles{datID};
+        load(fullfile(curFold,curFile));
+        
+        if ~exist('r','var')
+            r= RK{1}.r;
+        end
+        if ~exist('FPosition','var')
+            FPosition =x;
+        end
+        nr = length(r);
+        nframes = length(FPosition);
+        G = zeros(nr,nframes);
+        IsSig = zeros(nr,nframes);
+        
+        
+
+        FPosition= FPosition./(max(FPosition));
+
+        for k=1:length(RK)
+            cRK = RK{k};
+            curK = cRK.Obs';
+            minEnv = quantile(cRK.CSRSims,sigLvl/2)';
+            maxEnv = quantile(cRK.CSRSims,1-sigLvl/2)';
+            G(:,k) = log2(cRK.Obs./cRK.CSR)';
+            IsSig(:,k) = (curK<=minEnv | curK>=maxEnv);
+        end
+        
+        cntr =cntr+1;
+        maxG(cntr) = max(G(:));
+        minG(cntr) = min(G(:));
+
+        [fH,axM,axH] = RSRK_Plot(r,FPosition,G,'SigMap',IsSig,'cLims',cLims);
+        SaveFile = curFile(1:end-4);
+        fH.Units = 'centimeters';
+        fH.Position = [1 1 9.5 6.0];
+        
+        ylabel(axH,'Scale r (µm)')
+        yticklabels(axH,r)
+        yticklabels(axM,[]);
+        xlabel(axM,'Window Position')
+        xticklabels(axM,[]);
+        
+        % Save results
+        SaveName = fullfile(SaveDir, SaveFile);
+        saveas(fH,SaveName,'pdf')
+        print(fH,SaveName,'-dpng');
+        
+        close all
+        
+        clear r FPosition G IsSig
+    end
+end
